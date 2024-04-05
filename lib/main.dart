c1e1c81c55c78ffb0f5ee9a33acc934ca6f3b0ac
s1mpleNova <shahnaman632002@gmail.com>
2024-04-05 22:10:03 +0530

    in

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,69 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              GestureDetector(
-                onTap: () {
+              ElevatedButton(
+                onPressed: () {
                   final player = AudioPlayer();
                   player.play(AssetSource('audio/note1.wav'));
                   print('pressed');
                 },
-                child: Text('press me'),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                child: SizedBox(height: 20.0),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('audio/note2.wav'));
+                  print('pressed');
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                child: SizedBox.shrink(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('audio/note3.wav'));
+                  print('pressed');
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                child: SizedBox.shrink(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('audio/note4.wav'));
+                  print('pressed');
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                child: SizedBox.shrink(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('audio/note5.wav'));
+                  print('pressed');
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                child: SizedBox.shrink(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('audio/note6.wav'));
+                  print('pressed');
+                },
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                child: SizedBox.shrink(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('audio/note7.wav'));
+                  print('pressed');
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent),
+                child: SizedBox.shrink(),
               ),
             ],
           ),
